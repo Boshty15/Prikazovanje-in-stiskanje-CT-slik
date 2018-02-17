8f1126ca8ccdf92c04aeb74c6416fef917d1e21b
Boshty15 <bostjan@kostomaj.org>
2018-02-17 09:51:11 +0100

    Added

---
+++ Vaja1-CT/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Vaja1_CT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.bttLUT = new System.Windows.Forms.Button();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.bttCT = new System.Windows.Forms.Button();
+            this.bttStiskanje = new System.Windows.Forms.Button();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtBttDekod = new System.Windows.Forms.Button();
+            this.bttStiskanjeVec = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(482, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(512, 512);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(216, 903);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(257, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(234, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Back";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Location = new System.Drawing.Point(326, 41);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(75, 23);
+            this.buttonNext.TabIndex = 4;
+            this.buttonNext.Text = "Next";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // bttLUT
+            // 
+            this.bttLUT.Location = new System.Drawing.Point(234, 122);
+            this.bttLUT.Name = "bttLUT";
+            this.bttLUT.Size = new System.Drawing.Size(167, 23);
+            this.bttLUT.TabIndex = 5;
+            this.bttLUT.Text = "Load LUT ";
+            this.bttLUT.UseVisualStyleBackColor = true;
+            this.bttLUT.Click += new System.EventHandler(this.bttLUT_Click);
+            // 
+            // listView2
+            // 
+            this.listView2.Location = new System.Drawing.Point(1032, 9);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(229, 605);
+            this.listView2.TabIndex = 6;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.SelectedIndexChanged += new System.EventHandler(this.listView2_SelectedIndexChanged);
+            this.listView2.Click += new System.EventHandler(this.listView2_Click);
+            // 
+            // bttCT
+            // 
+            this.bttCT.Location = new System.Drawing.Point(234, 93);
+            this.bttCT.Name = "bttCT";
+            this.bttCT.Size = new System.Drawing.Size(167, 23);
+            this.bttCT.TabIndex = 7;
+            this.bttCT.Text = "Load CT";
+            this.bttCT.UseVisualStyleBackColor = true;
+            this.bttCT.Click += new System.EventHandler(this.bttCT_Click);
+            // 
+            // bttStiskanje
+            // 
+            this.bttStiskanje.Location = new System.Drawing.Point(234, 185);
+            this.bttStiskanje.Name = "bttStiskanje";
+            this.bttStiskanje.Size = new System.Drawing.Size(167, 23);
+            this.bttStiskanje.TabIndex = 8;
+            this.bttStiskanje.Text = "Stiskanje";
+            this.bttStiskanje.UseVisualStyleBackColor = true;
+            this.bttStiskanje.Click += new System.EventHandler(this.bttStiskanje_Click);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(234, 226);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Razmerje stiskanja: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(342, 226);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "0";
+            // 
+            // txtBttDekod
+            // 
+            this.txtBttDekod.Location = new System.Drawing.Point(234, 281);
+            this.txtBttDekod.Name = "txtBttDekod";
+            this.txtBttDekod.Size = new System.Drawing.Size(167, 23);
+            this.txtBttDekod.TabIndex = 14;
+            this.txtBttDekod.Text = "Dekodiranje";
+            this.txtBttDekod.UseVisualStyleBackColor = true;
+            this.txtBttDekod.Click += new System.EventHandler(this.txtBttDekod_Click);
+            // 
+            // bttStiskanjeVec
+            // 
+            this.bttStiskanjeVec.Location = new System.Drawing.Point(237, 358);
+            this.bttStiskanjeVec.Name = "bttStiskanjeVec";
+            this.bttStiskanjeVec.Size = new System.Drawing.Size(164, 23);
+            this.bttStiskanjeVec.TabIndex = 15;
+            this.bttStiskanjeVec.Text = "Stiskanje Več Slik";
+            this.bttStiskanjeVec.UseVisualStyleBackColor = true;
+            this.bttStiskanjeVec.Click += new System.EventHandler(this.bttStiskanjeVec_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1598, 920);
+            this.Controls.Add(this.bttStiskanjeVec);
+            this.Controls.Add(this.txtBttDekod);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.bttStiskanje);
+            this.Controls.Add(this.bttCT);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.bttLUT);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button bttLUT;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.Button bttCT;
+        private System.Windows.Forms.Button bttStiskanje;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button txtBttDekod;
+        private System.Windows.Forms.Button bttStiskanjeVec;
+    }
+}
+
---
+++ Vaja1-CT/Form1.cs
@@ -0,0 +1,488 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.DataFormats;
+
+namespace Vaja1_CT
+{
+    public partial class Form1 : Form
+    {
+        public const int size = 512;
+        Dictionary<int, string> code = new Dictionary<int, string>();
+
+        string pathtmp, pathLTmp;
+        short[,] slika_img = new short[size, size];
+        byte[,] lut = new byte[256, 3];
+        double lengthOriginal;
+        string fileName;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            code = setCode();
+
+        }
+        
+        public void ToBitMapMy(short[,] image, ref Bitmap bitImage)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    byte rgb = (byte)(image[i, j]);
+
+                    Color tmp = Color.FromArgb(255, rgb, rgb, rgb);
+                    bitImage.SetPixel(i, j, tmp);
+                }
+            }
+        }
+        public void ToBitMapMyColor(byte[,] lut, short[,] image, ref Bitmap bitImage)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    byte rgb = (byte)(image[i, j]);
+
+
+                    Color tmp = Color.FromArgb(255, lut[rgb, 0], lut[rgb, 1], lut[rgb, 2]);
+                    bitImage.SetPixel(i, j, tmp);
+                }
+            }
+        }
+
+        private void bttLUT_Click(object sender, EventArgs e)
+        {
+            openFileDialog2.Multiselect = false;
+            openFileDialog2.InitialDirectory = @"C:\Users\Bostjan\Documents\FERI\3.Letnik\Racunalnsika vecpredstavnost\Naloga1\Vaja1-CT\LUTS";
+            openFileDialog2.RestoreDirectory = false;
+            openFileDialog2.Filter = "LUT file (*.lut)|*.lut";
+
+            if (openFileDialog2.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                pathLTmp = Path.GetDirectoryName(openFileDialog2.FileName);
+                string[] fileEntries = Directory.GetFiles(pathLTmp);
+                foreach (string fileName in fileEntries)
+                    listView2.Items.Add(fileName);
+            }
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BinaryReader br;
+            label1.Text = "tekst";
+            if (listView1.SelectedItems.Count == 1)
+            {
+                bttStiskanje.Enabled = true;
+                string item = listView1.SelectedItems[0].Text;
+                lengthOriginal = new System.IO.FileInfo(item).Length;
+                label1.Text = item;
+                fileName = item.Substring(item.Length - 8, 4);
+                try
+                {
+                    br = new BinaryReader(File.Open(item, FileMode.Open));
+
+                    for (int i = 0; i < size; i++)
+                    {
+                        for (int j = 0; j < size; j++)
+                        {
+                            slika_img[i, j] = br.ReadInt16();
+
+                        }
+                    }
+                    Bitmap bitmap = new Bitmap(size, size);
+                    ToBitMapMy(slika_img, ref bitmap);
+                    bitmap.RotateFlip(RotateFlipType.Rotate90FlipX);
+                    pictureBox1.Image = bitmap;
+                }
+                catch
+                {
+
+                }
+
+            }
+           
+
+        }
+
+        private void bttCT_Click(object sender, EventArgs e)
+        {
+            label5.Text = "0";
+            //C:\Users\Bostjan\Documents\FERI\3.Letnik\Racunalnsika vecpredstavnost\Naloga1\Vaja1-CT\CT
+
+            openFileDialog1.Multiselect = false;
+            openFileDialog1.InitialDirectory = @"C:\Users\Bostjan\Documents\FERI\3.Letnik\Racunalnsika vecpredstavnost\Naloga1\Vaja1-CT\CT";
+            openFileDialog1.RestoreDirectory = false;
+            openFileDialog1.Filter = "IMG file (*.img)|*.img";
+
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                pathtmp = Path.GetDirectoryName(openFileDialog1.FileName);
+                string[] fileEntries = Directory.GetFiles(pathtmp);
+                foreach (string fileName in fileEntries)
+                    listView1.Items.Add(fileName);
+            }
+            
+        }
+
+        private void listView2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private Dictionary<int, string> setCode()
+        {
+            Dictionary<int, string> code = new Dictionary<int, string>();
+
+            // 2 BITA [-2,-1] && [1,2]
+            code[-2] = "0000";
+            code[-1] = "0001";
+            code[1] = "0010";
+            code[2] = "0011";
+
+            // 3 BITI [-6,-3] && [3,6]
+            code[-6] = "01000";
+            code[-5] = "01001";
+            code[-4] = "01010";
+            code[-3] = "01011";
+            code[6] = "01100";
+            code[5] = "01101";
+            code[4] = "01110";
+            code[3] = "01111";
+
+            // 4 BITI [-14,-7] && [7,14]
+            code[-14] = "100000";
+            code[-13] = "100001";
+            code[-12] = "100010";
+            code[-11] = "100011";
+            code[-10] = "100100";
+            code[-9] = "100101";
+            code[-8] = "100110";
+            code[-7] = "100111";
+            code[14] = "101111";
+            code[13] = "101110";
+            code[12] = "101101";
+            code[11] = "101100";
+            code[10] = "101011";
+            code[9] = "101010";
+            code[8] = "101001";
+            code[7] = "101000";
+
+            // 5 BITOV [-30,-15] && [15,30]
+            code[-30] = "1100000";
+            code[-29] = "1100001";
+            code[-28] = "1100010";
+            code[-27] = "1100011";
+            code[-26] = "1100100";
+            code[-25] = "1100101";
+            code[-24] = "1100110";
+            code[-23] = "1100111";
+            code[-22] = "1101000";
+            code[-21] = "1101001";
+            code[-20] = "1101010";
+            code[-19] = "1101011";
+            code[-18] = "1101100";
+            code[-17] = "1101101";
+            code[-16] = "1101110";
+            code[-15] = "1101111";
+            code[30] = "1111111";
+            code[29] = "1111110";
+            code[28] = "1111101";
+            code[27] = "1111100";
+            code[26] = "1111011";
+            code[25] = "1111010";
+            code[24] = "1111001";
+            code[23] = "1111000";
+            code[22] = "1110111";
+            code[21] = "1110110";
+            code[20] = "1110101";
+            code[19] = "1110100";
+            code[18] = "1110011";
+            code[17] = "1110010";
+            code[16] = "1110001";
+            code[15] = "1110000";
+            
+            return code;
+        }
+        private string Stiskanje(short pixel, ref int count, ref short prevPixel)
+        {
+            StringBuilder myString = new StringBuilder();
+
+            if(pixel != prevPixel && count != 0)
+            {
+                myString.Append("01" + Convert.ToString(count,2).PadLeft(6,'0'));
+                count = 0;
+            }
+            if(pixel == 0 && pixel != prevPixel)
+            {
+                prevPixel = pixel;
+                return myString.Append("11").ToString();
+            }
+            if(pixel == prevPixel)
+            {
+                count++;
+                if(count != 63)
+                {
+                    return null;
+                }
+                else
+                {
+                    myString.Append("01" + Convert.ToString(count,2).PadLeft(6,'0'));
+                    count = 0;
+                    return myString.ToString();
+                }
+
+
+            }
+            int razlika = pixel - prevPixel;
+            if(razlika <= 30 && razlika >= -30 && razlika != 0)
+            {
+                prevPixel = pixel;
+                return myString.Append("00" + code[razlika]).ToString();
+            }
+
+            prevPixel = pixel;
+            return myString.Append("10" + Convert.ToString(pixel, 2).PadLeft(12, '0')).ToString();
+        }
+
+        private void bttStiskanje_Click(object sender, EventArgs e)
+        {
+            //Stiskanje
+            StringBuilder myString = new StringBuilder();
+            short prevPixel = short.MinValue;
+            int count = 0;
+
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+
+                    myString.Append(Stiskanje((short)(slika_img[i, j] + 2048), ref count, ref prevPixel));
+                   
+                }
+            }
+            if(count > 0)
+            {
+                myString.Append("01");
+                myString.Append(Convert.ToString(count, 2).PadLeft(6, '0'));
+                count = 0;
+            }
+            
+
+            List<bool> boolList = new List<bool>();
+            for (int i = 0; i < myString.Length; i++)
+            {
+                if (myString[i].Equals('1'))
+                {
+                    boolList.Add(true);
+                }
+                else
+                {
+                    boolList.Add(false);
+                }
+            }
+
+            BitArray bit = new BitArray(boolList.ToArray());
+            byte[] byteFile = new byte[(int)Math.Ceiling((double)myString.Length / 8)];
+            bit.CopyTo(byteFile, 0);
+            File.WriteAllBytes(fileName + ".cmp", byteFile);
+            
+            double length = new System.IO.FileInfo(fileName + ".cmp").Length;
+
+            double razmerje = lengthOriginal / length;
+
+            label5.Text = razmerje.ToString("#.######");
+
+        }
+        private void txtBttDekod_Click(object sender, EventArgs e)
+        {
+            byte[] kodirano;
+            pictureBox1.Image = null;
+
+            // Dekoiranje
+            OpenFileDialog of = new OpenFileDialog();
+            of.Multiselect = false;
+            of.InitialDirectory = @"C:\Users\Bostjan\Documents\FERI\3.Letnik\Racunalnsika vecpredstavnost\Naloga1\Vaja1-CT\Vaja1-CT\bin\Debug\";
+            of.RestoreDirectory = false;
+            of.Filter = "CMP file (*.cmp)|*.cmp";
+
+            if (of.ShowDialog() == DialogResult.OK)
+            {
+                kodirano = File.ReadAllBytes(of.FileName);
+            }
+            else
+            {
+                return;
+            }
+
+            BitArray newBitArray = new BitArray(kodirano);
+
+            StringBuilder result = new StringBuilder(string.Empty);
+            for (int i = 0; i < newBitArray.Length; i++)
+            {
+                if (newBitArray[i])
+                {
+                    result.Append("1");
+                }
+                else
+                {
+                    result.Append("0");
+                }
+            }
+            
+            List<short> listShort = new List<short>();
+            short[,] newPicture2 = new short[size, size];
+
+            //int x = 0;
+            int intBit = 0; 
+            for (; listShort.Count < 262144; )
+            {
+
+                string tmp = result.ToString(intBit, 2);
+                intBit += 2;
+                if (tmp == "11")
+                {
+                    listShort.Add(-2048);
+                    continue;
+                }
+                if (tmp == "10")
+                {
+                    tmp = result.ToString(intBit, 12);
+                    intBit += 12;
+                    listShort.Add(Convert.ToInt16(tmp, 2));
+                    
+                    continue;
+                }
+                if (tmp == "00")
+                {
+                    StringBuilder codeBit = new StringBuilder();
+                    tmp = result.ToString(intBit, 2);
+                    intBit += 2;
+                    switch (tmp)
+                    {
+                        case "00":
+                            codeBit.Append("00");
+                            tmp = result.ToString(intBit, 2);
+                            codeBit.Append(tmp);
+                            intBit += 2;
+                            break;
+                        case "01":
+                            codeBit.Append("01");
+                            tmp = result.ToString(intBit, 3);
+                            codeBit.Append(tmp);
+                            intBit += 3;
+                            break;
+                        case "10":
+                            codeBit.Append("10");
+                            tmp = result.ToString(intBit, 4);
+                            codeBit.Append(tmp);
+                            intBit += 4;
+                            break;
+                        case "11":
+                            codeBit.Append("11");
+                            tmp = result.ToString(intBit, 5);
+                            codeBit.Append(tmp);
+                            intBit += 5;
+                            break;
+                    }
+                    int[] keysByValue = code.Where(xx => xx.Value == codeBit.ToString()).Select(pair => pair.Key).ToArray();
+                    listShort.Add((short)(listShort.Last() + keysByValue[0]));
+                    continue;
+                }
+                if (tmp == "01")
+                {
+                    tmp = result.ToString(intBit, 6);
+                    for (int k = 0; k < Convert.ToInt32(tmp, 2); k++)
+                    {
+                        listShort.Add(listShort.Last());
+                    }
+                    intBit += 6;
+                }
+
+
+            }
+            int m = 0;
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    newPicture2[i, j] = (short)(listShort[m]);
+                    m++;
+
+                }
+            }
+
+
+            MessageBox.Show("Dekodiranje uspešno!");
+
+            Bitmap bitmap = new Bitmap(size, size);
+            ToBitMapMy(newPicture2, ref bitmap);
+            bitmap.RotateFlip(RotateFlipType.Rotate90FlipX);
+            pictureBox1.Image = bitmap;
+
+        }
+
+        private void bttStiskanjeVec_Click(object sender, EventArgs e)
+        {
+            //Stiskanje več slik
+
+
+        }
+
+        private void listView2_Click(object sender, EventArgs e)
+        {
+            BinaryReader br;
+            //label1.Text = "tekst";
+            if (listView2.SelectedItems.Count > 0)
+            {
+                string item = listView2.SelectedItems[0].Text;
+                label1.Text = item;
+                try
+                {
+                    br = new BinaryReader(File.Open(item, FileMode.Open));
+                    // lut = null;
+                    for (int i = 0; i < 256; i++)
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            lut[i, j] = br.ReadByte();
+
+                        }
+                    }
+                    Bitmap bitmap = new Bitmap(size, size);
+
+                    ToBitMapMyColor(lut, slika_img, ref bitmap);
+                    bitmap.RotateFlip(RotateFlipType.Rotate90FlipX);
+
+                    pictureBox1.Image = bitmap;
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+    }
+}
+
